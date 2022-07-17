3ab1e81d94d71f66da8a3ba8225b95f5a2000328
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-17 13:36:24 -0500

    First commit.

---
+++ Space Race/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Space Race
+//
+//  Created by Camilo Hernández Guerrero on 17/07/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+}
